c0a778b0c6261ed8cce08079e11d63a9c1061d52
Vanessa Ating <atingenkay@gmail.com>
2017-09-12 11:38:57 +0100

    added the post route for adding new task

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//dependencies required for the app
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
@@ -8,11 +9,20 @@ app.set("view engine", "ejs");
 //placeholders for added task
 var task = ["buy socks", "practise with nodejs"];
 
+//post route for adding new task 
+app.post("/addtask", function(req, res) {
+    var newTask = req.body.newtask;
+    //add the new task from the post route
+    task.push(newTask);
+    res.redirect("/");
+});
+
+//render the ejs and display added task
 app.get("/", function(req, res) {
     res.render("index", { task: task });
 });
 
-
+//set app to listen on port 3000
 app.listen(3000, function() {
     console.log("server is running on port 3000");
 });
