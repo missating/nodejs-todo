a2b7aad9cb0e2f43ab5de12fb8d3c5578eb213ba
Vanessa Ating <atingenkay@gmail.com>
2017-09-20 12:48:54 +0100

    Updated the app to include remove task

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(express.static("public"));
 
 //placeholders for added task
 var task = ["buy socks", "practise with nodejs"];
+//placeholders for removed task
+var complete = ["finish jquery"];
 
 //post route for adding new task 
 app.post("/addtask", function(req, res) {
@@ -19,9 +21,25 @@ app.post("/addtask", function(req, res) {
     res.redirect("/");
 });
 
-//render the ejs and display added task
+app.post("/removetask", function(req, res) {
+    var completeTask = req.body.check;
+    //check for the "typeof" the different completed task, then add into the complete task
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
+
+//render the ejs and display added task, completed task
 app.get("/", function(req, res) {
-    res.render("index", { task: task });
+    res.render("index", { task: task, complete: complete });
 });
 
 //set app to listen on port 3000
